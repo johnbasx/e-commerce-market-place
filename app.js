665d4514c7a55030af6bf7dc27731187fb2301e5
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-04-08 21:09:11 -0400

    make it better

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
