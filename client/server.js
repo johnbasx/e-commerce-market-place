4f47ee5051a921e356cce813c2fa5d1566060563
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-04-08 22:52:55 -0400

    make it better

--- client/server.js
+++ client/server.js
@@ -7,7 +7,7 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8000;
