2e7d216561347bd32880f61ae21caaa72b5c890a
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-04-10 00:12:34 -0400

    deploy on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-Web: node app.js
+Web: node server.js
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("/client/build"));
+  }
+  
+  // Send every request to the React app
+  // Define any API routes before this runs
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
