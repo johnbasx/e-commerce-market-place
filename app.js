435cd0f3b0b34f990829f458b23bdd9077b832e1
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-04-09 22:44:40 -0400

    make it better

--- app.js
+++ app.js
@@ -17,12 +17,9 @@ const orderRoutes = require("./routes/order");
 // app
 const app = express();
 
-const port = process.env.PORT || 8001;
-
-
 // db
 mongoose
-    .connect(process.env.mongodb_uri, {
+    .connect(process.env.database, {
         useNewUrlParser: true,
         useCreateIndex: true
     }|| "mongodb://user:password>@ds141188.mlab.com:41188/heroku_vpklbm6m")
@@ -43,28 +40,9 @@ app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
+const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
-
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
-  
-  // Send every request to the React app
-  // Define any API routes before this runs
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
-  });
-
-
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
 });
 
--- client/server.js
+++ client/server.js
@@ -4,13 +4,13 @@ const app = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("build"));
   }
   
   // Send every request to the React app
   // Define any API routes before this runs
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    res.sendFile(path.join(__dirname, "build/index.html"));
   });
 
 const PORT = process.env.PORT || 3000;
