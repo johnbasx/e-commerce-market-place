ba2c8287e73bd604b83240ce687ae845c017cec9
Amitabh Kumar <daizyvats@gmail.com>
2020-04-11 14:19:09 -0400

    add

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use('/api', orderRoutes);
 
 const port = process.env.PORT || 8000;
 
-
+app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
