94005772233c680e044319444cd549837d8c3a01
Amitabh Kumar <daizyvats@gmail.com>
2020-04-14 12:53:54 -0400

    add

--- client/server.js
+++ client/server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const compression = require('compression');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const compression = require('compression');
+// const path = require('path');
+// const app = express();
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(compression());
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`App is running on port ${PORT}`);
+// });
--- server.js
+++ server.js
@@ -61,14 +61,14 @@ app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//   });
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
   
-// };
+};
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
